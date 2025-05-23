47c3a09f6518adc5bea5b52785e61054063c04ff
jinia91 <creee77@gmail.com>
2025-05-23 13:42:58 +0900

    spring ai, local llm 기본 챗봇

---
+++ src/main/kotlin/deepdive/ai/spring_ai_kata/SpringAiKataApplication.kt
@@ -0,0 +1,11 @@
+package deepdive.ai.spring_ai_kata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiKataApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAiKataApplication>(*args)
+}
---
+++ src/test/kotlin/deepdive/ai/spring_ai_kata/SpringAiKataApplicationTests.kt
@@ -0,0 +1,13 @@
+package deepdive.ai.spring_ai_kata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiKataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
